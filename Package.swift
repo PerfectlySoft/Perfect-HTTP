531d81032be4ad84fbe1f9c0cad13f7477215e78
Kyle Jessup <badthing@gmail.com>
2016-09-06 15:34:16 -0400

    Pinned dep versions

--- Package.swift
+++ Package.swift
@@ -1,3 +1,22 @@
+//
+//  Package.swift
+//  PerfectHTTP
+//
+//  Created by Kyle Jessup on 2016-05-02.
+//	Copyright (C) 2016 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
 import PackageDescription
 
 #if os(Linux)
@@ -5,9 +24,9 @@ let package = Package(
 	name: "PerfectHTTP",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", versions: Version(0,0,0)..<Version(10,0,0)),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", versions: Version(0,0,0)..<Version(10,0,0)),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-LinuxBridge.git", versions: Version(0,0,0)..<Version(10,0,0))
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-LinuxBridge.git", majorVersion: 2, minor: 0)
 	],
 	exclude: []
 )
@@ -16,8 +35,8 @@ let package = Package(
 	name: "PerfectHTTP",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", versions: Version(0,0,0)..<Version(10,0,0)),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", versions: Version(0,0,0)..<Version(10,0,0))
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", majorVersion: 2, minor: 0)
 	],
 	exclude: []
 )
