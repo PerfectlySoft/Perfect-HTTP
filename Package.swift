92394dd482d6924b9a2eab93d5de59b9a2fb1d8f
Kyle Jessup <badthing@gmail.com>
2016-09-06 11:48:58 -0400

    Added LinuxBridge dep

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
 import PackageDescription
 
+#if os(Linux)
+let package = Package(
+	name: "PerfectHTTP",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", versions: Version(0,0,0)..<Version(10,0,0)),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", versions: Version(0,0,0)..<Version(10,0,0)),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-LinuxBridge.git", versions: Version(0,0,0)..<Version(10,0,0))
+	],
+	exclude: []
+)
+#else
 let package = Package(
 	name: "PerfectHTTP",
 	targets: [],
@@ -9,3 +21,4 @@ let package = Package(
 	],
 	exclude: []
 )
+#endif
